934ade02c5633266c356735df50e05d8e5c2a793
Nahuel <nborda@infoxel.com>
2020-09-04 16:22:51 -0300

    patch dependencies and missing background-wallpaper

--- to-do-taller/.cache/sync-requires.js
+++ to-do-taller/.cache/sync-requires.js
@@ -5,9 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/binux/personal/high-end-taller/to-do-taller/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/home/binux/personal/high-end-taller/to-do-taller/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/home/binux/personal/high-end-taller/to-do-taller/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/home/binux/personal/high-end-taller/to-do-taller/src/pages/page-2.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/binux/personal-dev/high-end-taller/to-do-taller/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/home/binux/personal-dev/high-end-taller/to-do-taller/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/binux/personal-dev/high-end-taller/to-do-taller/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/home/binux/personal-dev/high-end-taller/to-do-taller/src/pages/page-2.js")))
 }
 
