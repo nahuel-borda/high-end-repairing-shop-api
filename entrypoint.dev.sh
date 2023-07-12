be02f306a32a54974a4f072b5db19b5309e76076
Nahuel Borda <bordanah@gmail.com>
2023-07-12 17:31:48 -0300

    general update

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -3,3 +3,4 @@ rm -f db.sqlite3;
 python manage.py makemigrations;
 python manage.py migrate --run-syncdb;
 python manage.py dumb_db_population;
+python manage.py runserver 0.0.0.0:8000;
--- main_service/devices/views.py
+++ main_service/devices/views.py
@@ -9,11 +9,6 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
-class HelloWorld(APIView):
-    permission_classes = [IsAuthenticated]
-    
-    def get(self, request, format=None):
-        return JsonResponse({'message': 'Hello, World!'})
 
 class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
 	permission_classes = [IsAuthenticatedOrReadOnly]
